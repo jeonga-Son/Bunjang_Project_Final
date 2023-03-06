5b7a8cadcaa53861e9d095c88986066f0905789b
jeonga-Son <jjeong0106@gmail.com>
2023-03-06 20:51:56 +0900

    [feat] User 구현

--- src/main/java/com/example/demo/src/user/model/User.java
+++ src/main/java/com/example/demo/src/user/model/User.java
@@ -1,16 +1,45 @@
+
+
 package com.example.demo.src.user.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.sql.Timestamp;
+import java.util.Date;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class User {
     private int userIdx;
-    private String ID;
-    private String userName;
-    private String password;
-    private String email;
+
+    private String name;
+
+    private String phoneNo;
+
+    private Date birthday;
+
+    private String address;
+
+    private Float latitude;
+
+    private Float longitude;
+
+    @JsonFormat(pattern = "yyyy.MM.dd'T'hh:mm", timezone = "Asia/Seoul")
+    private Timestamp createAt;
+
+    @JsonFormat(pattern = "yyyy.MM.dd'T'hh:mm", timezone = "Asia/Seoul")
+    private Timestamp updateAt;
+
+    private String status;
+
+    private String profileImgUrl;
+
+    private String shopDescription;
+
 }
