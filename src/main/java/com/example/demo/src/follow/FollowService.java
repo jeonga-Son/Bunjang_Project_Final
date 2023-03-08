5971bafb60aa9a002fc868bcd6635831269b24b0
minnie <radic7700@gmail.com>
2023-03-08 23:37:46 +0900

    [feat] 리뷰 쓰기 API 추가

--- src/main/java/com/example/demo/src/follow/FollowService.java
+++ src/main/java/com/example/demo/src/follow/FollowService.java
@@ -4,7 +4,6 @@ package com.example.demo.src.follow;
 import com.example.demo.config.BaseException;
 import com.example.demo.src.follow.*;
 import com.example.demo.src.follow.model.*;
-import com.example.demo.src.product.model.PatchProductRes;
 import com.example.demo.utils.JwtService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
