c96c76434b4410f3dee8e7e4b66e07b2560403b2
jeonga-Son <jjeong0106@gmail.com>
2023-03-09 09:45:09 +0900

    [feat] Chat model 구현

---
+++ src/main/java/com/example/demo/src/chat/model/Chat.java
@@ -0,0 +1,26 @@
+package com.example.demo.src.chat.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Chat {
+    private int chatIdx;
+
+    private int chatRoomIdx;
+
+    private String message;
+
+    private Timestamp createAt;
+
+    private Timestamp updateAt;
+
+    private String status;
+}
